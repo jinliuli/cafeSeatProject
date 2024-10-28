447fb2fd30d40cb0ec93a24d0b7593748161085a
jinliuli <jinliuli0305@gmail.com>
2024-10-29 08:35:06 +0900

    맵 마커이미지 추가 및 마이페이지 추가

--- CafeSeatProject/src/main/webapp/assets/js/cafe/map.js
+++ CafeSeatProject/src/main/webapp/assets/js/cafe/map.js
@@ -127,7 +127,7 @@
 			                <div class="info">
 								<div class="info-title">
 			                	<h5>${places.place_name}</h5>							
-									<input type="submit" class="btntitle" value="예약" data-value="${places.place_name}">
+									<input type="submit" id="btntitle${index+1}" class="btntitle" value="예약" data-value="${places.place_name}">
 								</div>`;
 			
 			    if (places.road_address_name) {
@@ -148,12 +148,17 @@
 			
 			// 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
 			function addMarker(position, idx, title) {
-				var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
+				//var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
+				var imageSrc = '../../images/location-pointer.png',
 					imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
 					imgOptions =  {
-						spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-						spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-						offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+						// spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+						// spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+						// offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+
+						spriteSize : new kakao.maps.Size(36, 37),
+						spriteOrigin : new kakao.maps.Point(0, 0),
+						offset: new kakao.maps.Point(18, 37)
 					},
 					markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
 						marker = new kakao.maps.Marker({
@@ -225,13 +230,20 @@
 			}
 
 
-
+			
 						
-			//예약번튼 보기btntitle
-			$(document).on('click', '.btntitle', function(){
-				alert('예약버튼 클릭이벤트');
-			});
+			//예약버튼 보기btntitle
+			for (let i=1; i<=15; i++){
+				$(document).on('click', '#btntitle'+i, function(){
+					alert($('#btntitle'+i).data('value'));
+					
+				});
+			}
+			
+			
+			
 
+			
 			 
 
 
