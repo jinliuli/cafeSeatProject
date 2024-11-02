071ac4e38154b41576e8fe5edd6f6f52cf2db7d6
bristolparkway mobile <sangsongsoo@gmail.com>
2024-11-02 14:20:32 +0900

    11/2/24 상수: OrderDAO 생성자 메서드 추가

--- CafeSeatProject/src/main/java/com/test/cafe/order/repository/OrderDAO.java
+++ CafeSeatProject/src/main/java/com/test/cafe/order/repository/OrderDAO.java
@@ -1,11 +1,29 @@
 package com.test.cafe.order.repository;
 
-public class OrderDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import com.test.cafe.menu.repository.MenuDAO;
+import com.test.util.DBUtil;
+
+public class OrderDAO {
+	
+	private static MenuDAO dao;
+	
+	private Connection conn;
+	private Statement stat;
+	private PreparedStatement pstat;
+	private ResultSet rs;
+
+	public OrderDAO() {
+		
 		//개인 테스트용은 cafe로 통일!
 		//this.conn = DBUtil.open("localhost", "cafe", "java1234");
 		
 		//프로젝트 공용
 		this.conn = DBUtil.open("52.78.251.201", "cafe", "java1234");
 
+	}
 }
