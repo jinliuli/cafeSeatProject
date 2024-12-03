40c9fc4303a7ff268ceac725f0ccf0c99381f0e7
yujin0510 <yuyujinkim65@gmail.com>
2024-12-03 12:05:12 +0900

    Ojdbc버전 수정 및 카페 메뉴 페이지 일부 수정

--- CafeSeatProject/src/main/java/com/test/cafe/menu/repository/MenuDAO.java
+++ CafeSeatProject/src/main/java/com/test/cafe/menu/repository/MenuDAO.java
@@ -25,10 +25,10 @@ public class MenuDAO {
 	private MenuDAO() {
 
 		//개인 테스트용은 cafe로 통일!
-		//this.conn = DBUtil.open("localhost", "cafe", "java1234");
+		this.conn = DBUtil.open("localhost", "cafe", "java1234");
 		
 		//프로젝트 공용
-		this.conn = DBUtil.open("52.78.251.201", "cafe", "java1234");
+		//this.conn = DBUtil.open("52.78.251.201", "cafe", "java1234");
 		
 		
 	}//MenuDAO DB연동 -----------------------------
